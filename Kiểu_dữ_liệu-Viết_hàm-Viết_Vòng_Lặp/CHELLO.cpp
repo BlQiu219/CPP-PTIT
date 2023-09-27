3d84a294c6d5cc3b451c32825f6bac9192403299
Blqiu <nguyenththu219@gmail.com>
2023-09-27 21:55:45 +0700

    qiu updated for the first time

---
+++ Kiểu_dữ_liệu-Viết_hàm-Viết_Vòng_Lặp/CHELLO.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CHELLO-Hello_worlf
+//
+//  Created by Nguyễn Thị Hoài Thu on 22/09/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello PTIT.";
+    return 0;
+}
